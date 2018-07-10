5b3bd97f6745542ce3487385767cb950a6a30a47
Serge Fonville <serge.fonville@gmail.com>
2018-07-10 13:29:46 +0200

    Not yet working, but rewriting to only use objects for adding/updating/retrieving

---
+++ davide.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+db.drop_all()
+db.create_all()
+
+u = User(username='susan', email='susan@example.com')
+db.session.add(u)
+
+p = Post(body='this is my post!', user_id=1)
+
+db.session.add(p)
+
+# Now I create a new post for a non-existing user and I'd expect an error...
+p = Post(body='this is my post!', user_id=2)
+ 
